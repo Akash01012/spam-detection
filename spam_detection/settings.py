4a0d84e8476861c5f4b78135d176b88787504512
akash kumar <akash639673@gmail.com>
2025-12-29 17:35:11 +0530

    update user serializers

--- spam_detection/settings.py
+++ spam_detection/settings.py
@@ -109,10 +109,14 @@ AUTH_USER_MODEL = 'users.User'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.AllowAny",
+    ),
 }
+
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
--- users/serializers.py
+++ users/serializers.py
@@ -2,12 +2,27 @@ from rest_framework import serializers
 from .models import User, Contact, SpamMark, GlobalContact
 
 
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['id', 'username', 'phone_number', 'email']
+
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'phone_number', 'email']
+        fields = ['id', 'username', 'phone_number', 'email', 'password']
 
-
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            phone_number=validated_data['phone_number'],
+            email=validated_data.get('email'),
+        )
+        user.set_password(validated_data['password'])  # 🔥 CRITICAL LINE
+        user.save()
+        return user
 class ContactSerializer(serializers.ModelSerializer):
     spam_likelihood = serializers.IntegerField(source="global_contact.spam_likelihood", read_only=True)
 
